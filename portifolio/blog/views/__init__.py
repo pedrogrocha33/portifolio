f325d780b38a2ea6f4a2fb1507d2e6fa1e0a1f47
pedrogrocha33 <pedro2022bds@icloud.com>
2024-05-20 06:56:41 -0300

    upload atualizacoes temlpate

--- portifolio/blog/views/__init__.py
+++ portifolio/blog/views/__init__.py
@@ -1,1 +1,2 @@
 from .post_views import PostView
+from .post_views import PostDetail
--- portifolio/blog/views/post_views.py
+++ portifolio/blog/views/post_views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
 from django.views import generic
 
-class PostView(generic.View):
+from blog.models import Post
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('<h1>Hello World by Cristiano Tobias!</h1>')
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
